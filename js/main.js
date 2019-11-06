3aff1f8af68f42fa5ffd6b8a5445cdb4c3645757
Marquiese Solomon <blaksnoopy@Marquieses-MacBook-Pro.local>
2019-11-05 17:34:43 -0800

    finish lab

--- js/main.js
+++ js/main.js
@@ -4,9 +4,8 @@ $('#addSkill').on('click', function(evt) {
     let skill = $('.text').val();
     newSkills.append(`<li><button class='button'>X</button>${skill}</li>`);
     $('.text').val('');
-    return false;
 });
 
-$('ul').on('click', 'button', function(evt) {
+newSkills.on('click', 'button', function(evt) {
     $(this).closest('li').remove();
 });
